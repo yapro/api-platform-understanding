964b5e8f079164c2620c696c3600922470b285de
lebedenko <lebedenko@banki.ru>
2021-09-27 00:22:40 +0300

    App\Kernel -> YaPro\ApiPlatformUnderstanding\Kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace YaPro\ApiPlatformUnderstanding;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
