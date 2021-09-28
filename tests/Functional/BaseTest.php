93f090501d5382fa8ee256d6ae4b4ef3357d5d88
lebedenko <lebedenko@banki.ru>
2021-09-28 19:39:29 +0300

    Первый тест - улучшение

--- tests/Functional/BaseTest.php
+++ tests/Functional/BaseTest.php
@@ -3,14 +3,31 @@ declare(strict_types=1);
 
 namespace YaPro\ApiPlatformUnderstanding\Tests\Functional;
 
+use Doctrine\ORM\EntityManagerInterface;
+use YaPro\DoctrineExt\ReloadDatabaseTrait;
 use YaPro\SymfonyHttpClientExt\HttpClientJsonLdExtTrait;
 use YaPro\SymfonyHttpTestExt\BaseTestCase;
 
 class BaseTest extends BaseTestCase
 {
 	use HttpClientJsonLdExtTrait;
+    use ReloadDatabaseTrait;
 
-	public function testCreateBook(): int
+    protected static EntityManagerInterface $entityManager;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        self::$entityManager = static::getContainer()->get(EntityManagerInterface::class);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        self::truncateAllTablesInSqLite();
+    }
+
+    public function testCreateBook(): int
 	{
 		$json = '
 		{
@@ -19,7 +36,22 @@ class BaseTest extends BaseTestCase
 		  "publicationDate": "2021-06-27T05:39:19.583Z"
 		}
 		';
-		$this->postLd('/api/books', $this->getJsonHelper()->jsonDecode($json, true));
+		$crawler = $this->postLd('/api/books', $this->getJsonHelper()->jsonDecode($json, true));
+		$this->assertJsonResponse('
+		{
+		  "@context": "\/api\/contexts\/Book",
+		  "@id": "\/api\/books\/1",
+		  "@type": "Book",
+		  "id": 1,
+		  "isbn": "string",
+		  "title": "string",
+		  "publicationDate": "2021-06-27T05:39:19+00:00",
+		  "reviews": []
+		}
+		');
+		// https://github.com/api-platform/api-platform/blob/main/api/tests/Api/GreetingsTest.php
 		return $this->assertResourceIsCreated();
 	}
+
+
 }
